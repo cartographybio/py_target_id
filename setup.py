f2cf9aff342f2cde3b47be9dd2ed16fcfc90f23c
jeffmgranja <106113190+jgranja-cartographybio@users.noreply.github.com>
2025-09-24 14:28:24 -0700

    update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="target_id",
+    name="py_target_id",
     version="0.1.0",
     author="Jeffrey Granja",
     author_email="jgranja@cartography.bio",
