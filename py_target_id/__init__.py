416b085e3e14becd5190d5a85ae91851039dbf4b
jeffmgranja <106113190+jgranja-cartographybio@users.noreply.github.com>
2025-09-26 11:56:26 -0700

    Update __init__.py

--- py_target_id/__init__.py
+++ py_target_id/__init__.py
@@ -7,14 +7,3 @@ from Google Cloud Storage for genomics workflows.
 
 __version__ = "0.1.0"
 __author__ = "Jeffrey Granja"
-
-# Import main functions to make them available at package level
-from .manifest import load_manifest, set_google_copy_version
-from .utils import google_copy
-
-# Define what gets imported with "from target_id import *"
-__all__ = [
-    'load_manifest',
-    'set_google_copy_version', 
-    'google_copy',
-]
