ce282b5f2f9f69b68c0767eb0893e2df771f8a88
jeffmgranja <106113190+jgranja-cartographybio@users.noreply.github.com>
2025-10-02 10:47:51 -0700

    Update __init__.py

--- py_target_id/run/__init__.py
+++ py_target_id/run/__init__.py
@@ -3,3 +3,4 @@ Core analysis pipelines for target identification.
 Includes malignancy detection and target ID workflow execution.
 """
 from .target_id_v1 import *
+from .target_id_multi_v1 import *
