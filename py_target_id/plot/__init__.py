773791d81141fd4cbdd6dea2e9fbf3635c999e92
jeffmgranja <106113190+jgranja-cartographybio@users.noreply.github.com>
2025-10-03 13:12:15 -0700

    Update __init__.py

--- py_target_id/plot/__init__.py
+++ py_target_id/plot/__init__.py
@@ -2,6 +2,9 @@
 Data handling and format conversion utilities.
 Includes tools for converting between H5/zarr formats and working with AnnData objects.
 """
+from . import single
+from . import multi
+
 from .py2r import *
 from .jg_plotnine import *
 from .utils import *
