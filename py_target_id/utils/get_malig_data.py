6e6c69108da566f9819cefd91ff176de5a2ddc51
jeffmgranja <106113190+jgranja-cartographybio@users.noreply.github.com>
2025-10-08 21:30:28 -0700

    Create get_malig_data.py

---
+++ py_target_id/utils/get_malig_data.py
@@ -0,0 +1,28 @@
+"""
+Reference data loading functions.
+"""
+
+__all__ = ['get_malig_adata', 'get_malig_archr_adata']
+
+def get_malig_adata(manifest):
+    import scanpy as sc
+    import anndata as ad
+    from tqdm import tqdm
+    
+    # Load all files
+    adata_list = []
+    for file_path in tqdm(manifest['Local_AD_Malig'], desc="Loading files"):
+        adata = sc.read_h5ad(file_path)
+        adata_list.append(adata)
+
+    # Concatenate
+    return ad.concat(adata_list, axis=0)
+
+def get_malig_archr_adata(manifest):
+    from py_target_id import data
+    malig_adata = data.read_h5(manifest.Local_Archr_Malig, "RNA")
+    malig_meta = ~malig_adata.obs_names.str.contains("nonmalig")
+    malig_adata = malig_adata[malig_meta, :]
+    return malig_adata
+
+
