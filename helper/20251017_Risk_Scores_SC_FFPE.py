c1df27d8c0249ea63d828928bdc9efa8a0cd1f4f
jeffmgranja <106113190+jgranja-cartographybio@users.noreply.github.com>
2025-10-17 14:07:01 -0700

    update

--- helper/20251017_Risk_Scores_SC_FFPE.py
+++ helper/20251017_Risk_Scores_SC_FFPE.py
@@ -32,6 +32,16 @@ results = tid.run.compute_ref_risk_scores(
 
 results.to_parquet('SC_Multi_Risk_Scores.20251017.parquet', engine='pyarrow', compression=None)
 
+#Single
+all_results = []
+gene_pairs = [(gene, gene) for gene in tid.utils.valid_genes()]
+for i in range(0, len(gene_pairs), 5000):
+    all_results.append(tid.run.compute_ref_risk_scores(ref_adata=ref_sc, type="SC", gene_pairs=gene_pairs[i:i+5000], device='cuda', batch_size=5000))
+risk_single_sc = pd.concat(all_results, ignore_index=True)
+risk_single_sc.loc[:,"gene_name"] = risk_single_sc['pair_name'].str.split('_', expand=True)[0]
+risk_single_sc = risk_single_sc[['gene_name', 'hazard_weighted_risk']].copy()
+risk_single_sc.to_parquet('SC_Single_Risk_Scores.20251017.parquet', engine='pyarrow', compression=None)
+
 #########################################################################################################
 #########################################################################################################
 import py_target_id as tid
@@ -65,3 +75,14 @@ results = tid.run.compute_ref_risk_scores(
 )
 
 results.to_parquet('FFPE_Multi_Risk_Scores.20251017.parquet', engine='pyarrow', compression=None)
+
+#Single
+all_results = []
+gene_pairs = [(gene, gene) for gene in tid.utils.valid_genes()]
+for i in range(0, len(gene_pairs), 5000):
+    all_results.append(tid.run.compute_ref_risk_scores(ref_adata=ref_ff, type="FFPE", gene_pairs=gene_pairs[i:i+5000], device='cuda', batch_size=5000))
+risk_ffpe_sc = pd.concat(all_results, ignore_index=True)
+risk_ffpe_sc.loc[:,"gene_name"] = risk_ffpe_sc['pair_name'].str.split('_', expand=True)[0]
+risk_ffpe_sc = risk_ffpe_sc[['gene_name', 'hazard_weighted_risk']].copy()
+risk_ffpe_sc.to_parquet('FFPE_Single_Risk_Scores.20251017.parquet', engine='pyarrow', compression=None)
+
